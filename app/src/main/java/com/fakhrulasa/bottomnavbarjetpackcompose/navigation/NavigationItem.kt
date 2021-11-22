cfa9e1d4333cf9085d48019b3cda63abd2e45f71
fakhrul <fakhrul.alam@btracsl.com>
2021-11-22 17:20:56 +0600

    -navigation controller -screen -theme updated

--- app/src/main/java/com/fakhrulasa/bottomnavbarjetpackcompose/navigation/NavigationItem.kt
+++ app/src/main/java/com/fakhrulasa/bottomnavbarjetpackcompose/navigation/NavigationItem.kt
@@ -5,5 +5,5 @@ import com.fakhrulasa.bottomnavbarjetpackcompose.R
 sealed class NavigationItem(var route:String,var icon:Int, var title:String){
     object Home: NavigationItem("home", R.drawable.ic_baseline_home_24,"Home")
     object Group: NavigationItem("group", R.drawable.ic_baseline_group_24,"Group")
-    object Setting: NavigationItem("setting", R.drawable.ic_baseline_home_24,"Setting")
+    object Setting: NavigationItem("setting", R.drawable.ic_baseline_settings_24,"Setting")
 }
