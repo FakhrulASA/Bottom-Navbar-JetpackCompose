5a07984cf7e8d8876c6257797a9fd96c6ea27890
fakhrul <fakhrul.alam@btracsl.com>
2021-11-22 13:23:35 +0600

    -navigation controller -screen

--- app/src/main/java/com/fakhrulasa/bottomnavbarjetpackcompose/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/fakhrulasa/bottomnavbarjetpackcompose/ui/screen/HomeScreen.kt
@@ -1,16 +1,20 @@
 package com.fakhrulasa.bottomnavbarjetpackcompose.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
@@ -22,13 +26,95 @@ fun HomeScreenView() {
             .background(Color.White)
             .wrapContentSize(Alignment.Center)
     ) {
-        Text(
-            text = "Home View",
-            fontWeight = FontWeight.Bold,
-            color = Color.Black,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 25.sp
-        )
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp)
+                .clickable{ },
+            elevation = 10.dp
+        ) {
+            Column(
+                modifier = Modifier.padding(15.dp)
+            ) {
+                Text(
+                    buildAnnotatedString {
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.W900, color = Color(0xFF4552B6),fontSize = 20.sp)
+                        ) {
+                            append("Welcome to Home")
+                        }
+                    }
+                )
+                Text(
+                    buildAnnotatedString {
+                        append("Now you are in the ")
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.W900, color = Color(0xFF4552B6),fontSize = 20.sp)) {
+                            append("This is the home page of the view, Here you will see home")
+                        }
+
+                    }
+                )
+            }
+        }
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp)
+                .clickable{ },
+            elevation = 10.dp
+        ) {
+            Column(
+                modifier = Modifier.padding(15.dp)
+            ) {
+                Text(
+                    buildAnnotatedString {
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.W900, color = Color(0xFF4552B6),fontSize = 20.sp)
+                        ) {
+                            append("Welcome to Home")
+                        }
+                    }
+                )
+                Text(
+                    buildAnnotatedString {
+                        append("Now you are in the ")
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.W900, color = Color(0xFF4552B6),fontSize = 20.sp)) {
+                            append("This is the home page of the view, Here you will see home")
+                        }
+
+                    }
+                )
+            }
+        }
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp)
+                .clickable{ },
+            elevation = 10.dp
+        ) {
+            Column(
+                modifier = Modifier.padding(15.dp)
+            ) {
+                Text(
+                    buildAnnotatedString {
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.W900, color = Color(0xFF4552B6),fontSize = 20.sp)
+                        ) {
+                            append("Welcome to Home")
+                        }
+                    }
+                )
+                Text(
+                    buildAnnotatedString {
+                        append("Now you are in the ")
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.W900, color = Color(0xFF4552B6),fontSize = 20.sp)) {
+                            append("This is the home page of the view, Here you will see home")
+                        }
+
+                    }
+                )
+            }
+        }
     }
 }
